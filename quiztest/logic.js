68e9a70a0e2c746dce7d74dbfd141f790c985a06
Isabel Reinema <isabel.reinema@outlook.de>
2021-03-27 20:14:48 +0100

    quiz test

---
+++ quiztest/logic.js
@@ -0,0 +1,9 @@
+//variabeln festlegen
+function variables() {
+    localStorage.setItem('richtigeq1', 0);
+    localStorage.setItem('richtigeq2', 0);
+    localStorage.setItem('frageq1', 1);
+    localStorage.setItem('frageq2', 1);
+}
+
+//texte festlegen?
